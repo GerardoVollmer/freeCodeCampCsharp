be4d16856e67531957a23122ad03c3f97860ba04
GerardoVollmer <geravollmer17@gmail.com>
2026-05-06 09:51:57 -0300

    Juego con if elseif else

--- Program.cs
+++ Program.cs
@@ -20,13 +20,18 @@ Console.WriteLine("Hello, Gerardito Vollmer! tengo {0} años", edad);*/
 
 
 // Ejercicio 1: Incorporación de una instrucción if para mostrar diferentes mensajes según el valor de la variable total
-//Ejeercicio 2: Incorporación de otra instrucción if para implementar puntos extra por obtener dobles
+// Ejercicio 2: Incorporación de otra instrucción if para implementar puntos extra por obtener dobles
 //              "Si dos dados cualesquiera muestran el mismo valor, se obtendrán dos puntos extra por obtener dobles"
-Random numAl = new Random(); // Instancia de la clase Random para generar números aleatorios
+// Ejercicio 3: Creación de una lógica de decisión anidada con if, else if y else
+/*Random numAl = new Random(); // Instancia de la clase Random para generar números aleatorios
 
-int num1 = numAl.Next(1, 7); // Número entre 1 y 6 (inclusive)
-int num2 = numAl.Next(1, 7); 
-int num3 = numAl.Next(1, 7); 
+int num1 = numAl.Next(6); // Número entre 0 y 5 (inclusive)
+int num2 = numAl.Next(6); 
+int num3 = numAl.Next(5); 
+
+num1 = 6;
+num2 = 6;
+num3 = 6;
 
 int sumTotal = num1 + num2 + num3;
 
@@ -34,34 +39,38 @@ Console.WriteLine($"Numero 1: {num1} + \nNumero 2: {num2} + \nNumero 3: {num3} =
 
 if ((num1 == num2) || (num1 == num3) || (num2 == num3)) //denomina condición compuesta
 {
-    Console.WriteLine("¡Doble! Has ganado +2 puntos extra.");
-    sumTotal += 2;
-}
-if ((num1 == num2) && (num2 == num3))
-{
-    Console.WriteLine("¡Triple! Has ganado +6 puntos extra.");
-    sumTotal += 6;
+    if ((num1 == num2) && (num2 == num3))
+    {
+        Console.WriteLine("¡Triple acierto! Has ganado +6 puntos extra.");
+        sumTotal += 6;
+    }
+    else
+    {
+        Console.WriteLine("¡Doble acierto! Has ganado +2 puntos extra.");
+         sumTotal += 2;
+    }
+
+    Console.WriteLine($"Your total including the bonus extra points is: {sumTotal}");
 }
 //---------------------------------------------------------------------
-if (sumTotal >= 15)
+if (sumTotal >= 16)
 {
-    Console.WriteLine("Haz ganado el primer premio");
+    Console.WriteLine("You win a new car!");
 }
-if (sumTotal < 15)
+else if (sumTotal >= 10)
 {
-    Console.WriteLine("Sorry, You lose.");
+    Console.WriteLine("You win a new laptop!");
 }
-
-
-/*
-// Expresion booleana con string.Contains(), evalua si una cadena contiene otra
-string message = "the quick brown fox jump over the lazy dog.";
-bool result = message.Contains("fox");
-
-Console.WriteLine(result);
-
-if (message.Contains("fox"))
+else if (sumTotal == 7)
 {
-    Console.WriteLine("What does the fox say?");
+    Console.WriteLine("You win a trip for two!");
 }
+else
+{
+    Console.WriteLine("You win a kitten(gatito)!");
+}
+
 */
+
+//Ejercicio: Realización de una actividad 
+//de desafío para aplicar reglas de negocio
